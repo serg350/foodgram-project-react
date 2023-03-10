48ad400a53191f32b5e180d7832157cf3135fa64
Sergey Bobrov <serg.bobrik@mail.ru>
2023-03-10 22:55:32 +0300

    Review 1.3

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -9,9 +9,9 @@ from recipes.models import Favorite, Recipes, RecipesIngredient, ShoppingCart
 from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
-
 from tags.models import Tags
 from users.models import Follower
+
 from foodgram.settings import INVALID_USERNAMES
 
 User = get_user_model()
