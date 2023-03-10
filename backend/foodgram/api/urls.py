930edcdac310b415074dfdff473eed0dcb8a3ca7
Sergey Bobrov <serg.bobrik@mail.ru>
2023-03-10 22:47:09 +0300

    Review 1.2

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,7 +1,8 @@
-from api.views import (CustomUserListView, IngredientsListView,
-                       RecipesListView, TagsListView)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from api.views import (CustomUserListView, IngredientsListView,
+                       RecipesListView, TagsListView)
 
 api_v1_router = DefaultRouter()
 api_v1_router.register(r'recipes', RecipesListView, basename='recipes')
