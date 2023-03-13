ec3c6c34faa6e559322774d198eca619421c483d
Sergey Bobrov <serg.bobrik@mail.ru>
2023-03-13 19:57:04 +0300

    Review 3.1

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -47,7 +47,7 @@ class Recipes(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
-        ordering = ('-name',)
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.name
